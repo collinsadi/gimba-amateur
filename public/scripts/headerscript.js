d9c1601ad65c3324923110c7a9d93f7325c43388
mrhardy23 <collinsadi20@gmail.com>
2023-05-20 14:36:32 +0100

    added preloader to the page

--- public/scripts/headerscript.js
+++ public/scripts/headerscript.js
@@ -2,6 +2,17 @@
 const openNav = document.querySelector('#open-btn');
 const closeNav = document.querySelector("#close-menu");
 const Panel = document.querySelector("#mobile-panel");
+const preloader = document.getElementById('preloader');
+
+
+window.addEventListener('load', ()=>{
+
+    setTimeout(() => {
+
+        preloader.style.display = "none"
+        
+    }, 2000);
+})
 
 
 openNav.addEventListener("click", ()=>{
