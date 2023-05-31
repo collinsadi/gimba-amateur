4195b8bafd73dc2a26cf18882b5a644c45c9e466
mrhardy23 <collinsadi20@gmail.com>
2023-05-31 15:27:36 +0100

    styled dashboard header and sidebar

---
+++ public/scripts/dashboardscript.js
@@ -0,0 +1,11 @@
+
+
+let bringSidebar = document.querySelector('#mobile-sidebar');
+let sideBar = document.querySelector('#dashboard-sidebar');
+
+
+bringSidebar.addEventListener('click', ()=>{
+   // alert('working')
+    sideBar.style.display = "block"
+
+})
