076bf44ef3fe5083290e21aec276d60b1f9b2bb1
mrhardy23 <collinsadi20@gmail.com>
2023-06-10 17:29:02 +0100

    created a page for user profile and it is working perfectly and if the user is not found the it is redirected to the 404 page, I am super Excited Tho, I feel ;like the new Instagram or reddit boss Lol ):

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -377,4 +377,31 @@ try {
    })
 
 
+// Profile page
+
+router.get('/u/:username', async(req, res)=>{
+
+  const useridname = req.params.username
+
+  try{
+
+    const user = await User.findOne({useridname})
+
+    if(!user){
+
+      // res.status(401).json({details: "User Not Found"})
+      res.status(404).render("404")
+      return;
+    }
+
+    res.status(200).send(user)
+
+  }catch(error){
+    console.log(error)
+    res.status(500).json("Internal Server Error")
+  }
+
+
+})
+
 module.exports = router;
